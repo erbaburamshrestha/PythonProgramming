30684712f2f8256c986d72fee4a271579cb5fb9e
baburam1997 <shrestha.baburam1997@gmail.com>
2025-02-16 15:55:30 +0545

    some ipynb files are added to preset with my friends in research techniques class

---
+++ src/ME-CMP-Python/caculator.py
@@ -0,0 +1,19 @@
+# A simple module, calculator.py
+def add(x, y):
+   return (x+y)
+
+
+def subtract(x, y):
+   return (x-y)
+
+
+def multiply(x, y):
+   return (x*y)
+
+
+def divide(x, y):
+    if y == 0:
+         return "Error! Division by zero."
+    else:
+         return (x/y)
+
